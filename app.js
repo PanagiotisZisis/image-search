81771e44520387c8b6738f25cc2a0c45f1cc07f4
Panagiotis Zisis <takis-zissis@hotmail.com>
2017-04-21 19:11:50 +0300

    Almost ready

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var cors = require('cors');
+var bing = require('node-bing-api') ({accKey: "5c9191867ba14d98a5923efd6909a114"});
+var mongo = require('mongodb').MongoClient;
+
+var app = express();
+
+app.use(cors());
+
+app.get('/imagesearch/:value', function(req, res) {
+    var value = req.params.value;
+    var offset = req.query.offset;
+
+    bing.images(value, {top: 10, skip: offset}, function(error, response, body) {
+        if (error) {
+            res.send('There was an error.');
+        }
+       var result = [];
+       body['value'].forEach(function(item) {
+        result.push(
+            {
+                url: item.contentUrl,
+                snippet: item.name,
+                thumbnail: item.thumbnailUrl,
+                context: item.hostPageDisplayUrl
+            }
+        );
+       });
+       mongo.connect('mongodb://localhost:27017/latest', function(err, db) {
+           if (err) {
+               res.send('There was an error connecting to the database.');
+           }
+           var searches = db.collection('searches');
+           var date = new Date();
+           var obj = {
+               term: value,
+               when: date
+           };
+           searches.insert(obj);
+           db.close();
+       });
+       res.json(result);
+    });
+});
+
+app.get('/latest', function(req, res) {
+    mongo.connect('mongodb://localhost:27017/latest', function(err, db) {
+        if (err) {
+            res.send('There was an error connecting to the database.');
+        }
+        var searches = db.collection('searches');
+        searches.find({}, {_id: 0}).toArray(function(err, result) {
+            if (err) {
+                res.send('There was an error reading the data from the database.');
+            }
+            res.json(result);
+        });
+        db.close();
+    });
+});
+
+app.listen(8000, function() {
+    console.log('App is working...');
+})
